8571d92394c14ff7abf8e76201223f09c00a25d3
Casey Silver <cls2be@virginia.edu>
2019-08-25 20:25:29 -0400

    Finish QR code generation functionality

--- background_script.js
+++ background_script.js
@@ -1,1 +1,28 @@
-// Put all the javascript code here, that you want to execute in background.
+'use strict';
+
+var tabs = {};
+
+async function logSubject(request) {
+  if (request && request.url && request.type === "main_frame") {
+    try {
+      let securityInfo = await browser.webRequest.getSecurityInfo(request.requestId, {});
+      console.log(request);
+      console.log(securityInfo);
+      console.log(securityInfo.certificates[0].fingerprint.sha256);
+      tabs[request.tabId] = {
+        url: request.url,
+        fingerprint: securityInfo.certificates[0].fingerprint.sha256,
+        ip: request.ip
+      }
+    }
+    catch(error) {
+      console.error(error);
+    }
+  }
+}
+  
+browser.webRequest.onHeadersReceived.addListener(
+  logSubject,
+  {urls: ["<all_urls>"]},
+  ["responseHeaders", "blocking"]
+);
